fee43745bf162db651bc63c322f4b9db0ce3619d
Viresh <vireshfegade@gmail.com>
2022-04-05 21:47:07 +0530

    feat : add Masonry css for notes listing layout

--- src/frontend/components/NoteCard/NoteCard.jsx
+++ src/frontend/components/NoteCard/NoteCard.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import "./NoteCard.css";
 import parse from "html-react-parser";
 const NoteCard = ({ noteData }) => {
-  return <div className="ql-editor note-card m-4">{parse(`${noteData}`)}</div>;
+  return (
+    <div className="ql-editor note-card m-4 rounded-md">
+      {parse(`${noteData}`)}
+    </div>
+  );
 };
 
 export { NoteCard };
--- src/frontend/components/RichTextEditor/RichTextEditor.jsx
+++ src/frontend/components/RichTextEditor/RichTextEditor.jsx
@@ -3,10 +3,10 @@ import "react-quill/dist/quill.snow.css";
 import "./RichTextEditor.css";
 const RichTextEditor = () => {
   return (
-    <div className="d-flex flex-col new-note-wrapper m-8">
+    <div className="d-flex flex-col new-note-wrapper m-8 rounded-sm">
       <input
         type="text"
-        className="note-heading w-100 p-3 text-sm"
+        className="note-heading w-100 p-3 text-md font-wt-bold mt-2"
         placeholder="Title"
       />
       <QuillEditor />
