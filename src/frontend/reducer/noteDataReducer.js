c365650c7ac68abb91e33259ebe48aefc6cad672
Viresh <vireshfegade@gmail.com>
2022-04-06 14:31:45 +0530

    feat : add color to note

--- src/frontend/reducer/noteDataReducer.js
+++ src/frontend/reducer/noteDataReducer.js
@@ -4,14 +4,17 @@ const noteDataReducer = (noteData, { type, payload }) => {
       return { ...noteData, title: payload };
     case "SET_CONTENT":
       return { ...noteData, content: payload };
+    case "SET_COLOR":
+      return { ...noteData, bgColor: payload };
     case "RESET":
       return {
         title: "",
         content: "",
         createdAt: "",
+        bgColor: "",
       };
     default:
-      return noteData;
+      throw new Error("Unhandled action type");
   }
 };
 export { noteDataReducer };
